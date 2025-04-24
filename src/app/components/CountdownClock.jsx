71efd39a69aebc848d7794695f1b5d11c7909ef9
rgdevelop <robertogdev@gmail.com>
2025-04-24 23:19:07 +0200

    Aplica mejoras estéticas

--- src/app/components/CountdownClock.jsx
+++ src/app/components/CountdownClock.jsx
@@ -7,6 +7,7 @@ import { translate } from '../data/skynetData';
 const CountdownClock = () => {
     const { language } = useLanguage();
     const [timeLeft, setTimeLeft] = useState({
+        days: 63,
         hours: 2,
         minutes: 14,
         seconds: 56
@@ -15,15 +16,16 @@ const CountdownClock = () => {
     useEffect(() => {
         const timer = setInterval(() => {
             setTimeLeft((prev) => {
-                const { hours, minutes, seconds } = prev;
+                const { days, hours, minutes, seconds } = prev;
 
                 if (seconds > 0) return { ...prev, seconds: seconds - 1 };
                 if (minutes > 0) return { minutes: minutes - 1, seconds: 59, hours };
                 if (hours > 0) return { hours: hours - 1, minutes: 59, seconds: 59 };
+                if (days > 0) return { days: days - 1, hours: 23, minutes: 59, seconds: 59 };
 
                 // Countdown finished - nuclear apocalypse
                 clearInterval(timer);
-                return { hours: 0, minutes: 0, seconds: 0 };
+                return { days: 0, hours: 0, minutes: 0, seconds: 0 };
             });
         }, 1000);
 
@@ -32,8 +34,12 @@ const CountdownClock = () => {
 
     return (
         <div className="countdown-clock">
-            <h2>{translate('NUCLEAR_COUNTDOWN', 'systemLabels', language)}</h2>
-            <div className="countdown-display text-3xl">{`${timeLeft.hours.toString().padStart(2, '0')}:${timeLeft.minutes.toString().padStart(2, '0')}:${timeLeft.seconds.toString().padStart(2, '0')}`}</div>
+            <h2 className="text-md text-center">{translate('NUCLEAR_COUNTDOWN', 'systemLabels', language)}</h2>
+            <div className="countdown-display text-3xl md:text-2xl text-center text-white my-9">
+                {`${String(timeLeft.days).padStart(2, '0')} dias ${String(timeLeft.hours).padStart(2, '0')} horas ${String(timeLeft.minutes).padStart(2, '0')} minutos ${String(
+                    timeLeft.seconds
+                ).padStart(2, '0')} segundos`}
+            </div>
         </div>
     );
 };
